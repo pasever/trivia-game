9d73cf875538460d2b09cd651edacdc526379afb
Pasever <pas1034ever@gmail.com>
2017-09-30 01:07:46 -0400

    a.1.5

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# trivia-game
+Week 5 Boot Camp
+
+Trivia-game
+
+You can play my game here:
+https://pasever.github.io/trivia-game/index.html
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,10 @@ var incorrectAnswers = 0;
 var len = questions.length;
 var currentQuestion;
 var questionCounter = 0;
-var clickCounter = 0;
 var clickedButton;
 var gameOverBool = false;
 var totalAnswers = correctAnswers + incorrectAnswers;
+
 
 
 
@@ -17,7 +17,7 @@ $(".quiz").ready(function () {
 });
 
 
-$("#startingButton").on("click", function () {
+var x = $("#startingButton").on("click", function start() {
 
     $("#startingButton").hide();
     $("#startingText").hide();
@@ -28,38 +28,38 @@ $("#startingButton").on("click", function () {
 });
 
 function countdown() {
-    var seconds = 20;
+    var seconds = 15;
     var countdown = document.getElementById('questionTimer');
-    
+
 
     int = setInterval(function () { //Starts the interval
         seconds--;
         countdown.innerHTML = seconds;
         if (seconds === 0) {
             clearInterval(int); //Stops the interval
-            
+
             setTimeout(nextQuestion, 25);
         }
     }, 1000);
 }
 
 function generate() {
-    if ( gameOverBool === true ) {
+    if (gameOverBool === true) {
         return;
         reset();
-        
+
     }
     var rand = Math.floor(Math.random() * len);
-    
+
     currentQuestion = questions[rand];
     $("#questionPlace").html(currentQuestion.question);
     var ansLen = currentQuestion.answers.length;
-    
+
     for (var i = 0; i < ansLen; i++) {
         $("#" + i).append(currentQuestion.answers[i]).show();
     }
 
-    
+
     for (i; i < 4; i++) {
         var emptyButton = $("#" + i).hide();
     }
@@ -73,30 +73,23 @@ function generate() {
 
 $("label").on("click", function () {
     var guess = $(this)[0].innerText;
-    console.log(guess);
     correctGuess(guess);
-    clickCounter++;
-    console.log(clickCounter);
 
 });
 
 function correctGuess(choice) {
 
     var correctAnswer = currentQuestion.solution;
-    console.log(correctAnswer);
     if (choice === correctAnswer) {
         correctAnswers++;
-        console.log("Great Job!");
         alert("Great Job!");
-        console.log(correctAnswers);
+        //$("#correctPlace").html("Correct");
         clearInterval(int);
         reset();
         nextQuestion();
     } else {
         alert("Hmmmmmm");
-        console.log("Hmmmmm");
         incorrectAnswers++;
-        console.log(incorrectAnswers);
         clearInterval(int);
         reset();
         nextQuestion();
@@ -120,77 +113,30 @@ function reset() {
         countdown();
 
     }
+    
 
 }
 
 
 function checkIfGameEnd() {
     totalAnswers = correctAnswers + incorrectAnswers;
-    console.log(totalAnswers);
     if (totalAnswers === 10) {
         gameOverBool = true;
-        alert("Thank you for taking the test!");
         reset();
         $(".quiz").hide();
+        results();
+        $("#questionCounter").html(0);
 
     }
 }
 
-//    results();
+function results() {
 
-
-//    
-// //for loop around questions answers
-//  //i is iterator for question index
-//  //j is the iterator for the answers index
-//  for(var j = 0; j > questions[i].answers.length; j++){
-//    //appended buttons to the button div
-//    //id is j to determine value
-//    //doSomething passes over the question index and the answer index
-//    //  `   tick mark allows you to insert varaibles with a ${} around the variable
-//    // look up javascript tick marks to read more in it
-//    $("div").append(`<button id="${j}" onClick=doSomething(j, i)>${questions[i].answers[j]}</button>`)
-//  }
-//
-//  //accepts the answer index and question index
-//  doSomething(answer, question) {
-//    //determines solution from the questions solution and the answer the user chose
-//    if(questions[question].solution == questions[question].answers[answer]){
-//
-//    }
-//  }
-//
-//
-//questions[0].answers[1]
-//function generate () {
-//    $("#questionPlace").html(questions[1].question);
-//    $(myArray)[1].addClass('active');
-//    var questionsSomething = questions[0];
-//    console.log(questions[1].question);
-//    $("#answersPlace").html("<h1>Hello here</h1>");
-//}
-//var container = document.createElement("div");
-//for (var i = 0; i < questions.length; i++) {
-//    var questionsContainer = document.createElement("div");
-//    questionsContainerId = "questions" + 1;  
-//}
-// make a variable thats going to hold the question number
-// randomly select an element from questions array 
-// increment varibale that holds question number by one
-// remove that element from array
-// make html consisting of 
-// question text
-// possible answers
-// <button value="answer_value">answer_text</button>
-// register event handlers for possible answer buttons 
-// function(event) {
-//      var value_of_button_clicked = event.target.value // probably hold the value
-// }
-// user clicks button answer
-// compare value of button with value of correct answer
-// if it is the same
-// display message congrats || optional
-// move on to next question
-// else
-// display green border around correct answer
-// move on to next questions
+    var finalMessage = "Thank you for taking the test!";
+    finalMessage += "</br>" + "</br>" + "Questions you've answered correctly - " + correctAnswers + "</br>";
+    //finalMessage += "</br>" + "If you want to take the test again click below";
+    $("#questionPlace").html(finalMessage);
+    //$("#startingButton").show();
+    //$("#startingButton").on("click", x);
+    
+}
